ea77c29d0030f66fc82ef52205b89c21cd68eca4
oumaymabg <oumaymabou257@gmail.com>
2021-02-01 22:19:52 +0000

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
+""" Gather data from an API  """
 import requests
 from sys import argv
-"""
-Write a Python script that, using this REST API.
-"""
-
 
 if __name__ == "__main__":
-    """
-    display on the standard output the employee todo list
-    """
-    ID = int(argv[1])
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                        format(ID)).json()
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
-                        format(ID)).json()
-    tables = []
-    for table in todo:
-        if table.get('completed') is True:
-            tables.append(table.get('title'))
-    print("Employee {} is done with tasks({}/{}):".
-          format(user.get('name'), len(tables), len(todo)))
-    for table in tables:
-        print("\t {}".format(table))
+    url = 'https://jsonplaceholder.typicode.com'
+    employee_id = argv[1]
+    """ Extract Employee name """
+    user = requests.get('{}/users/{}'.format(url, employee_id))
+    employee_name = user.json().get('name')
+    """ Extract Employee Tasks """
+    todos = requests.get('{}/todos?userId={}'.format(url, employee_id))
+    result = todos.json()
+    """ Extract List of completed Tasks of Employee """
+    Done = [Task for Task in result if Task.get('completed')]
+    """ Result """
+    print('Employee {} is done with tasks({}/{}):'.
+          format(employee_name, len(Done), len(result)))
+    for Task in Done:
+        print('\t {}'.format(Task.get('title')))
