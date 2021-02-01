ea93348cb767904b5ebf5f48a5a74cb0f67e0f45
oumaymabg <oumaymabou257@gmail.com>
2021-02-01 19:33:12 +0000

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,11 +15,11 @@ if __name__ == "__main__":
                         format(ID)).json()
     todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
                         format(ID)).json()
-    tasks = []
-    for task in todo:
-        if task.get('completed') is True:
-            tasks.append(task.get('title'))
+    tables = []
+    for table in todo:
+        if table.get('completed') is True:
+            tables.append(table.get('title'))
     print("Employee {} is done with tasks({}/{}):".
-          format(user.get('name'), len(tasks), len(todo)))
-    for task in tasks:
-        print("\t {}".format(task))
+          format(user.get('name'), len(tables), len(todo)))
+    for table in tables:
+        print("\t {}".format(table))
