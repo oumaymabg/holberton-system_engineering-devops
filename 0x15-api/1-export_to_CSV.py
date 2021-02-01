02d460197e0f51a464542030e860ddf8dec3dab9
oumaymabg <oumaymabou257@gmail.com>
2021-02-01 22:21:32 +0000

    CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
+"""  Export to CSV  """
 import csv
 import requests
 from sys import argv
-import json
-"""
- Python script to export data in the CSV format.
-"""
-
 
 if __name__ == "__main__":
-    """
-    Export the data into CSV file
-    """
-    ID = int(argv[1])
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                        format(ID)).json()
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
-                        format(ID)).json()
-    with open("{}.csv".format(ID), 'w') as csvf:
-        filler = csv.writer(csvf, delimiter=',', quoting=csv.QUOTE_ALL)
-        for task in todo:
-            filler.writerow([ID, user.get('username'),
-                            task.get('completed'), task.get('title')])
+    url = 'https://jsonplaceholder.typicode.com'
+    USER_ID = argv[1]
+    """ Extract Employee name """
+    user = requests.get('{}/users/{}'.format(url, USER_ID)).json()
+    USERNAME = user.get('username')
+    """ Extract Employee Tasks """
+    todos = requests.get('{}/todos?userId={}'.format(url, USER_ID)).json()
+    rows = []
+    for Task in todos:
+        TASK_COMPLETED_STATUS = Task.get("completed")
+        TASK_TITLE = Task.get("title")
+        rows.append([USER_ID, USERNAME, TASK_COMPLETED_STATUS, TASK_TITLE])
+    filename = "%s.csv" % USER_ID
+    """ export to csv file """
+    with open(filename, 'wt') as f:
+        csv_writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        [csv_writer.writerow(row) for row in rows]
