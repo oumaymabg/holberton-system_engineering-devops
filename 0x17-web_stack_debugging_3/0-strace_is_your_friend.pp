3201298c300610d1243a42d1d5be1ba6862e3d46
oumaymabg <oumaymabou257@gmail.com>
2021-02-16 14:15:35 +0000

    stace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,2 +1,2 @@
 # Using strace find out  Apache 500 error ;Fixes wordpress config file
-exec { '/usr/bin/env sed -i "s/phpp/php/g" /var/www/html/wp-settings.php': }
+exec { '/usr/sbin/sshd -D -i "s/phpp/php/g" /var/www/html/wp-settings.php': }
