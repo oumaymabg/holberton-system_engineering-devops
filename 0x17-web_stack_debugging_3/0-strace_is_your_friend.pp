1c259684243830874abc19c15d6dfefd69bee7ce
oumaymabg <oumaymabou257@gmail.com>
2021-02-28 20:47:35 +0000

    strace_is_your_friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,2 +1,6 @@
 # Using strace find out  Apache 500 error ;Fixes wordpress config file
-exec { '/usr/sbin/sshd -D -i "s/phpp/php/g" /var/www/html/wp-settings.php': }
+exec { 'fix typo':
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
